eabbbe2d75726ecced34561f923cd81ad198118b
logicOnAbstractions <logiconabstractions@gmail.com>
2020-10-28 11:17:54 -0400

    yaml sandbox

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-from tensorflow import keras
-from tensorflow.keras import layers
-from kerastuner.tuners import RandomSearch
-import kerastuner.engine.hyperparameters as hp
+import yaml
+import pprint
 
+FILE = "dummy.yaml"
 
-def model_builder(hp):
-    model = keras.Sequential()
-    model.add(keras.layers.Flatten(input_shape=(28, 28)))
+def get_yaml():
+    """ reads a properly formatted yaml configuration file & parses it into python dictionary format  """
+    with open(FILE, 'r') as configs:
+        configs = yaml.load(configs, yaml.FullLoader)
+    return configs
 
-    # Tune the number of units in the first Dense layer
-    # Choose an optimal value between 32-512
-    hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
-    model.add(keras.layers.Dense(units=hp_units, activation='relu'))
-    model.add(keras.layers.Dense(10))
-
-    # Tune the learning rate for the optimizer
-    # Choose an optimal value from 0.01, 0.001, or 0.0001
-    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
-
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
-                  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    return model
-
-m = model_builder(hp)
+content = get_yaml()
+# print(content)
+pprint.pprint(content, indent=2, compact=True, width=5, sort_dicts=False)
